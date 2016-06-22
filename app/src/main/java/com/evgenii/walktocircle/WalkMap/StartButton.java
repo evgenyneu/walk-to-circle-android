fac814aebc034cf14416d585782afa366f1bb5f9
Evgenii Neumerzhitckii <sausageskin@gmail.com>
2016-06-22 12:13:46 +1000

    Animating start button

--- app/src/main/java/com/evgenii/walktocircle/WalkMap/StartButton.java
+++ app/src/main/java/com/evgenii/walktocircle/WalkMap/StartButton.java
@@ -1,15 +1,12 @@
 package com.evgenii.walktocircle.WalkMap;
 import com.evgenii.walktocircle.R;
 
-import android.animation.Animator;
 import android.animation.AnimatorInflater;
 import android.animation.AnimatorSet;
 import android.app.Activity;
 import android.view.View;
 import android.view.animation.Animation;
-import android.view.animation.Animation.AnimationListener;
 import android.view.animation.AnimationUtils;
-import android.widget.Button;
 import android.widget.ImageView;
 
 import com.evgenii.walktocircle.Libs.BounceInterpolator;
@@ -39,10 +36,15 @@ public class StartButton {
     }
 
     public void rotate180Degrees() {
+        rotateStartButton180DegreesAndFadeOut();
+        rotateRewindButton180DegreesAndFadeIn();
+    }
+
+    void rotateStartButton180DegreesAndFadeOut() {
         final View button = getStartImage();
 
         AnimatorSet animatorSet = (AnimatorSet) AnimatorInflater.loadAnimator(mActivity,
-                R.animator.rotate_start_button_180);
+                R.animator.rotate_180_degress_and_fade_out);
 
         BounceInterpolator interpolator = new BounceInterpolator(0.2, 7);
         animatorSet.setInterpolator(interpolator);
@@ -50,7 +52,25 @@ public class StartButton {
         animatorSet.start();
     }
 
-    public View getStartImage() {
+    void rotateRewindButton180DegreesAndFadeIn() {
+        final View button = getRewindImage();
+        button.setVisibility(View.VISIBLE);
+        button.setAlpha(0);
+
+        AnimatorSet animatorSet = (AnimatorSet) AnimatorInflater.loadAnimator(mActivity,
+                R.animator.rotate_180_degrees_and_fade_in);
+
+        BounceInterpolator interpolator = new BounceInterpolator(0.2, 7);
+        animatorSet.setInterpolator(interpolator);
+        animatorSet.setTarget(button);
+        animatorSet.start();
+    }
+
+    View getStartImage() {
         return (ImageView) mActivity.findViewById(R.id.startImage);
+    }
+
+    View getRewindImage() {
+        return (ImageView) mActivity.findViewById(R.id.rewindImage);
     }
 }
