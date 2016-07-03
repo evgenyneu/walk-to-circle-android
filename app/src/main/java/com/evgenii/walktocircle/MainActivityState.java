217c9bad04ee2fca7241c988698c769130c77e94
Evgenii Neumerzhitckii <sausageskin@gmail.com>
2016-07-03 15:20:46 +1000

    Walk screen

--- app/src/main/java/com/evgenii/walktocircle/MainActivityState.java
+++ app/src/main/java/com/evgenii/walktocircle/MainActivityState.java
@@ -10,18 +10,22 @@ import java.util.Set;
 // Saved state for the app
 public class MainActivityState {
     // Contains the location of the currently dropped pin. If null, pin is not currently dropped.
-    private LatLng currentPinLocation = null;
+    private LatLng mCurrentPinLocation = null;
 
     // If true then we show Congratulations fragment
-    private boolean showCongratulationsScreen = false;
+    private boolean mShowCongratulationsScreen = false;
 
     // The list of quotes already shown to the user. The list is maintained in order to avoid showing same quotes one after another.
-    private Set<String> alreadyShownQuotes;
+    private Set<String> mAlreadyShownQuotes;
+
+    private boolean mIsTutorialMode = true;
 
     static final String CURRENT_LOCATION_LATITUDE = "currentLocationLatitude";
     static final String CURRENT_LOCATION_LONGITUDE = "currentLocationLongitude";
     static final String SHOW_CONGRATULATIONS_SCREEEN = "showCongratulationsScreen";
     static final String ALREADY_SHOWN_QUOTES = "alreadyShownQuotes";
+    static final String IS_TUTORIAL_MODE = "isTutorialMode";
+
 
     private static MainActivityState mInstance;
 
@@ -41,29 +45,29 @@ public class MainActivityState {
         double currentLocationLongitude =  preferences.getFloat(CURRENT_LOCATION_LONGITUDE, 0);
 
         if (currentLocationLatitude != 0 && currentLocationLongitude != 0) {
-            currentPinLocation = new LatLng(currentLocationLatitude, currentLocationLongitude);
+            mCurrentPinLocation = new LatLng(currentLocationLatitude, currentLocationLongitude);
         }
 
-        showCongratulationsScreen = preferences.getBoolean(SHOW_CONGRATULATIONS_SCREEEN, false);
+        mShowCongratulationsScreen = preferences.getBoolean(SHOW_CONGRATULATIONS_SCREEEN, false);
 
-        alreadyShownQuotes = preferences.getStringSet(ALREADY_SHOWN_QUOTES, new HashSet<String>());
+        mAlreadyShownQuotes = preferences.getStringSet(ALREADY_SHOWN_QUOTES, new HashSet<String>());
     }
 
     private void saveState() {
         SharedPreferences preferences = MainActivity.instance.getPreferences(0);
         SharedPreferences.Editor editor = preferences.edit();
 
-        if (currentPinLocation == null) {
+        if (mCurrentPinLocation == null) {
             editor.putFloat(CURRENT_LOCATION_LATITUDE, 0);
             editor.putFloat(CURRENT_LOCATION_LONGITUDE, 0);
         } else {
-            editor.putFloat(CURRENT_LOCATION_LATITUDE, (float)currentPinLocation.latitude);
-            editor.putFloat(CURRENT_LOCATION_LONGITUDE, (float)currentPinLocation.longitude);
+            editor.putFloat(CURRENT_LOCATION_LATITUDE, (float)mCurrentPinLocation.latitude);
+            editor.putFloat(CURRENT_LOCATION_LONGITUDE, (float)mCurrentPinLocation.longitude);
         }
 
-        editor.putBoolean(SHOW_CONGRATULATIONS_SCREEEN, showCongratulationsScreen);
+        editor.putBoolean(SHOW_CONGRATULATIONS_SCREEEN, mShowCongratulationsScreen);
 
-        editor.putStringSet(ALREADY_SHOWN_QUOTES, alreadyShownQuotes);
+        editor.putStringSet(ALREADY_SHOWN_QUOTES, mAlreadyShownQuotes);
 
         editor.commit();
     }
@@ -72,12 +76,12 @@ public class MainActivityState {
     // -----------
 
     public LatLng getCurrentPinLocation() {
-        return currentPinLocation;
+        return mCurrentPinLocation;
     }
 
     public static void savePinLocation(LatLng pinLocation) {
         if (mInstance != null) {
-            mInstance.currentPinLocation = pinLocation;
+            mInstance.mCurrentPinLocation = pinLocation;
             mInstance.saveState();
         }
     }
@@ -86,12 +90,12 @@ public class MainActivityState {
     // -----------
 
     public Set<String> getAlreadyShownQuotes() {
-        return alreadyShownQuotes;
+        return mAlreadyShownQuotes;
     }
 
     public static void saveAlreadyShownQuotes(Set<String> shown) {
         if (mInstance != null) {
-            mInstance.alreadyShownQuotes = shown;
+            mInstance.mAlreadyShownQuotes = shown;
             mInstance.saveState();
         }
     }
