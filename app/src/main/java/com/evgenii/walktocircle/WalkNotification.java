4725df88149ddd754058a1633c9b2e091907d1a6
Evgenii Neumerzhitckii <sausageskin@gmail.com>
2016-07-06 17:20:01 +1000

    Show notification

--- app/src/main/java/com/evgenii/walktocircle/WalkNotification.java
+++ app/src/main/java/com/evgenii/walktocircle/WalkNotification.java
@@ -16,14 +16,17 @@ public class WalkNotification {
 
     public int mId = 1;
 
-    void sendNotification(String title, String contentText) {
-
+    void showNotification(String title, String contentText) {
         if (WalkApplication.isActive()) {
             // Do not show notification if the app is in the foreground.
             return;
         }
 
         Context context = WalkApplication.getAppContext();
+
+        Intent notificationIntent = new Intent(context, MainActivity.class);
+        notificationIntent.setFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP | Intent.FLAG_ACTIVITY_SINGLE_TOP);
+        PendingIntent intent = PendingIntent.getActivity(context, 0, notificationIntent, 0);
 
         Uri soundUri = Uri.parse("android.resource://" + WalkApplication.getAppContext().getPackageName() + "/" + R.raw.notification_morning_fresh);
 
@@ -34,26 +37,15 @@ public class WalkNotification {
                         .setContentText(contentText)
                         .setColor(ContextCompat.getColor(context, R.color.notificationBackgroundColor))
                         .setSound(soundUri)
+                        .setPriority(Notification.PRIORITY_MAX)
+                        .setAutoCancel(true) // Remove notification when it is touched. Also stops the notification sound.
+                        .setContentIntent(intent)
                         .setVibrate(new long[]{
                                 0, 100,
                                 100, 200,
                                 100, 200,
                                 100, 200});
 
-
-        Intent notificationIntent = new Intent(context, MainActivity.class);
-
-        notificationIntent.setFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP
-                | Intent.FLAG_ACTIVITY_SINGLE_TOP);
-
-        PendingIntent intent = PendingIntent.getActivity(context, 0,
-                notificationIntent, 0);
-
-        mBuilder.setContentIntent(intent);
-
-        mBuilder.setPriority(Notification.PRIORITY_MAX);
-
-        mBuilder.setAutoCancel(true);
 
         NotificationManager mNotificationManager =
                 (NotificationManager) context.getSystemService(Context.NOTIFICATION_SERVICE);
